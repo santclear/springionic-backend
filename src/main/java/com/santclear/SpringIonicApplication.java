027205e95728d40ba99210cd73506414a9493f41
Sant Clear Ali Costa <santclear@gmail.com>
2017-10-01 23:24:57 -0300

    Projeto criado

---
+++ src/main/java/com/santclear/SpringIonicApplication.java
@@ -0,0 +1,12 @@
+package com.santclear;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringIonicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringIonicApplication.class, args);
+	}
+}
