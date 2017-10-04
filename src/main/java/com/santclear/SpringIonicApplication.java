7b36b7d884d716c3b758d0a44e2307a4589b53d2
Sant Clear Ali Costa <santclear@gmail.com>
2017-10-03 23:27:13 -0300

    Implementado método run na classe principal para inicializar objetos e valores para aplicação, nele criado instâncias da Categoria e inserido-as no banco de dados

--- src/main/java/com/santclear/SpringIonicApplication.java
+++ src/main/java/com/santclear/SpringIonicApplication.java
@@ -1,12 +1,31 @@
 package com.santclear;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class SpringIonicApplication {
+import com.santclear.domain.Categoria;
+import com.santclear.repositories.CategoriaRepository;
 
+@SpringBootApplication
+public class SpringIonicApplication implements CommandLineRunner {
+
+	@Autowired
+	private CategoriaRepository categoriaRepository;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(SpringIonicApplication.class, args);
+	}
+
+	@Override
+	public void run(String... arg0) throws Exception {
+		
+		Categoria cat1 = new Categoria(null, "Informática");
+		Categoria cat2 = new Categoria(null, "Escritório");
+		
+		categoriaRepository.save(Arrays.asList(cat1, cat2));
 	}
 }
