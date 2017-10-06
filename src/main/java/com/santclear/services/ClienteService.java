9a598d028a61a36fadb67e88a594e51b1bc6d860
Sant Clear Ali Costa <santclear@gmail.com>
2017-10-06 20:54:01 -0300

    Implementado endpoint /clientes/{id} e as respectivas classes envolvidas

---
+++ src/main/java/com/santclear/services/ClienteService.java
@@ -0,0 +1,23 @@
+package com.santclear.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.santclear.domain.Cliente;
+import com.santclear.repositories.ClienteRepository;
+import com.santclear.services.exceptions.ObjectNotFoundException;
+
+@Service
+public class ClienteService {
+	
+	@Autowired
+	private ClienteRepository repo;
+	
+	public Cliente buscar(Integer id) {
+		Cliente obj = repo.findOne(id);
+		if(obj == null) {
+			throw new ObjectNotFoundException("Objeto não encontrado Id: "+ id +", Tipo: "+ Cliente.class.getName());
+		}
+		return obj;
+	}
+}
