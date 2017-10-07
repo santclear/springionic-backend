ed7085dab18054e5d86c601e8a60c7c3195876c0
Sant Clear Ali Costa <santclear@gmail.com>
2017-10-07 19:11:31 -0300

    Implementado endpoint para atualização de Categoria e alterado o nome do método buscar para find nas classes de serviços

--- src/main/java/com/santclear/services/CategoriaService.java
+++ src/main/java/com/santclear/services/CategoriaService.java
@@ -13,7 +13,7 @@ public class CategoriaService {
 	@Autowired
 	private CategoriaRepository repo;
 	
-	public Categoria buscar(Integer id) {
+	public Categoria find(Integer id) {
 		Categoria obj = repo.findOne(id);
 		if(obj == null) {
 			throw new ObjectNotFoundException("Objeto não encontrado Id: "+ id +", Tipo: "+ Categoria.class.getName());
@@ -23,6 +23,12 @@ public class CategoriaService {
 	
 	public Categoria insert(Categoria obj) {
 		obj.setId(null);
+		return repo.save(obj);
+	}
+	
+	public Categoria update(Categoria obj) {
+		find(obj.getId());
+		// O método save do Spring Data realiza operações de save e update. Se o id for nulo ele salva e se não for atualiza.
 		return repo.save(obj);
 	}
 }
