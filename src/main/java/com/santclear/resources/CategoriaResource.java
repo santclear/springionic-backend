6749fc22463a2ce646695738dd81d86cbe51f1ca
Sant Clear Ali Costa <santclear@gmail.com>
2017-10-07 18:53:42 -0300

    Implementado endpoint para inserção de Categoria

--- src/main/java/com/santclear/resources/CategoriaResource.java
+++ src/main/java/com/santclear/resources/CategoriaResource.java
@@ -1,11 +1,15 @@
 package com.santclear.resources;
 
+import java.net.URI;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.santclear.domain.Categoria;
 import com.santclear.services.CategoriaService;
@@ -21,5 +25,16 @@ public class CategoriaResource {
 	public ResponseEntity<?> find(@PathVariable Integer id) {
 		Categoria obj = service.buscar(id);
 		return ResponseEntity.ok().body(obj);
+	}
+	
+	/* Implementação seguindo o padrão W3C. Retorna estatus 201 que é referente a criação de objeto realizada com sucesso.
+	 * Segundo o W3C, se o objeto for criado com sucesso é boa prática retornar a URI do novo objeto criado
+	 * https://www.w3.org/Protocols/rfc2616/rfc2616-sec10.html
+	 * */
+	@RequestMapping(method=RequestMethod.POST)
+	public ResponseEntity<Void> insert(@RequestBody Categoria obj) {
+		obj = service.insert(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).build();
 	}
 }
