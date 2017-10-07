4f0a179f2af1d96cd859e5a2252a71279b424f4e
Sant Clear Ali Costa <santclear@gmail.com>
2017-10-07 03:13:47 -0300

    Criado Pedido, EstadoPagamento e Pagamento

--- src/main/java/com/santclear/SpringIonicApplication.java
+++ src/main/java/com/santclear/SpringIonicApplication.java
@@ -1,5 +1,6 @@
 package com.santclear;
 
+import java.text.SimpleDateFormat;
 import java.util.Arrays;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,13 +13,20 @@ import com.santclear.domain.Cidade;
 import com.santclear.domain.Cliente;
 import com.santclear.domain.Endereco;
 import com.santclear.domain.Estado;
+import com.santclear.domain.Pagamento;
+import com.santclear.domain.PagamentoComBoleto;
+import com.santclear.domain.PagamentoComCartao;
+import com.santclear.domain.Pedido;
 import com.santclear.domain.Produto;
+import com.santclear.domain.enums.EstadoPagamento;
 import com.santclear.domain.enums.TipoCliente;
 import com.santclear.repositories.CategoriaRepository;
 import com.santclear.repositories.CidadeRepository;
 import com.santclear.repositories.ClienteRepository;
 import com.santclear.repositories.EnderecoRepository;
 import com.santclear.repositories.EstadoRepository;
+import com.santclear.repositories.PagamentoRepository;
+import com.santclear.repositories.PedidoRepository;
 import com.santclear.repositories.ProdutoRepository;
 
 @SpringBootApplication
@@ -36,6 +44,10 @@ public class SpringIonicApplication implements CommandLineRunner {
 	private ClienteRepository clienteRepository;
 	@Autowired
 	private EnderecoRepository enderecoRepository;
+	@Autowired
+	private PedidoRepository pedidoRepository;
+	@Autowired
+	private PagamentoRepository pagamentoRepository;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(SpringIonicApplication.class, args);
@@ -84,5 +96,21 @@ public class SpringIonicApplication implements CommandLineRunner {
 		
 		clienteRepository.save(Arrays.asList(cli1));
 		enderecoRepository.save(Arrays.asList(e1, e2));
+		
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy hh:mm");
+		Pedido ped1  = new Pedido(null, sdf.parse("30/09/2017 10:32"), cli1, e1);
+		Pedido ped2  = new Pedido(null, sdf.parse("10/10/2017 19:35"), cli1, e2);
+		
+		Pagamento pagto1 = new PagamentoComCartao(null, EstadoPagamento.QUITADO, ped1, 6);
+		ped1.setPagamento(pagto1);
+		
+		Pagamento pagto2 = new PagamentoComBoleto(null, EstadoPagamento.PENDENTE, ped2, sdf.parse("20/10/2017 00:00"), null);
+		ped2.setPagamento(pagto2);
+		
+		cli1.getPedidos().addAll(Arrays.asList(ped1, ped2));
+		
+		pedidoRepository.save(Arrays.asList(ped1, ped2));
+		
+		pagamentoRepository.save(Arrays.asList(pagto1, pagto2));
 	}
 }
