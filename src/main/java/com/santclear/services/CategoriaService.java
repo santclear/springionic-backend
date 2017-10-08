56e4838940f6d12294f95b67a3287682cc5ec354
Sant Clear Ali Costa <santclear@gmail.com>
2017-10-08 14:38:51 -0300

    Implementado validação sintática com Bean Validation de Categoria ao salvar o atualizar

--- src/main/java/com/santclear/services/CategoriaService.java
+++ src/main/java/com/santclear/services/CategoriaService.java
@@ -10,6 +10,7 @@ import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.santclear.domain.Categoria;
+import com.santclear.dto.CategoriaDTO;
 import com.santclear.repositories.CategoriaRepository;
 import com.santclear.services.exceptions.DataIntegrityException;
 import com.santclear.services.exceptions.ObjectNotFoundException;
@@ -56,5 +57,9 @@ public class CategoriaService {
 	public Page<Categoria> findPage(Integer page, Integer linesPerPage, String orderBy, String direction) {
 		PageRequest pageRequest = new PageRequest(page, linesPerPage, Direction.valueOf(direction), orderBy);
 		return repo.findAll(pageRequest);
+	}
+	
+	public Categoria fromDTO(CategoriaDTO objDTO) {
+		return new Categoria(objDTO.getId(), objDTO.getNome());
 	}
 }
