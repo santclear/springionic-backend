85b01298cf4d90f4c591ee61b6c33b42ca76f42a
Sant Clear Ali Costa <santclear@gmail.com>
2017-10-08 02:07:59 -0300

    Implementado CategoriaDTO e endpoint para buscar todas as categorias no formato CategoriaDTO

--- src/main/java/com/santclear/resources/CategoriaResource.java
+++ src/main/java/com/santclear/resources/CategoriaResource.java
@@ -1,6 +1,8 @@
 package com.santclear.resources;
 
 import java.net.URI;
+import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -12,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.santclear.domain.Categoria;
+import com.santclear.dto.CategoriaDTO;
 import com.santclear.services.CategoriaService;
 
 @RestController
@@ -49,5 +52,12 @@ public class CategoriaResource {
 	public ResponseEntity<Void> delete(@PathVariable Integer id) {
 		service.delete(id);
 		return ResponseEntity.noContent().build();
+	}
+	
+	@RequestMapping(method=RequestMethod.GET)
+	public ResponseEntity<List<CategoriaDTO>> findAll() {
+		List<Categoria> list = service.findAll();
+		List<CategoriaDTO> listDTO = list.stream().map(obj -> new CategoriaDTO(obj)).collect(Collectors.toList());
+		return ResponseEntity.ok().body(listDTO);
 	}
 }
