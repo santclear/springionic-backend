2954e81f9619a14e3ecc2a3f2e09301b11632806
Sant'Clear <santclear@gmail.com>
2018-02-02 10:45:04 -0200

    Inserindo Pedido

--- src/main/java/com/santclear/domain/PagamentoComCartao.java
+++ src/main/java/com/santclear/domain/PagamentoComCartao.java
@@ -2,28 +2,30 @@ package com.santclear.domain;
 
 import javax.persistence.Entity;
 
+import com.fasterxml.jackson.annotation.JsonTypeName;
 import com.santclear.domain.enums.EstadoPagamento;
 
 @Entity
+@JsonTypeName("pagamentoComCartao")
 public class PagamentoComCartao extends Pagamento {
 	
 	private static final long serialVersionUID = 1L;
 	
-	private Integer numeroParcelas;
+	private Integer numeroDeParcelas;
 
 	public PagamentoComCartao() {}
 
-	public PagamentoComCartao(Integer id, EstadoPagamento estado, Pedido pedido, Integer numeroParcelas) {
+	public PagamentoComCartao(Integer id, EstadoPagamento estado, Pedido pedido, Integer numeroDeParcelas) {
 		super(id, estado, pedido);
-		this.numeroParcelas = numeroParcelas;
+		this.numeroDeParcelas = numeroDeParcelas;
 
 	}
 
-	public Integer getNumeroParcelas() {
-		return numeroParcelas;
+	public Integer getNumeroDeParcelas() {
+		return numeroDeParcelas;
 	}
 
-	public void setNumeroParcelas(Integer numeroParcelas) {
-		this.numeroParcelas = numeroParcelas;
+	public void setNumeroDeParcelas(Integer numeroDeParcelas) {
+		this.numeroDeParcelas = numeroDeParcelas;
 	}
 }
--- src/main/java/com/santclear/services/PedidoService.java
+++ src/main/java/com/santclear/services/PedidoService.java
@@ -1,10 +1,18 @@
 package com.santclear.services;
 
+import java.util.Date;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.santclear.domain.ItemPedido;
+import com.santclear.domain.PagamentoComBoleto;
 import com.santclear.domain.Pedido;
+import com.santclear.domain.enums.EstadoPagamento;
+import com.santclear.repositories.ItemPedidoRepository;
+import com.santclear.repositories.PagamentoRepository;
 import com.santclear.repositories.PedidoRepository;
+import com.santclear.repositories.ProdutoRepository;
 import com.santclear.services.exceptions.ObjectNotFoundException;
 
 @Service
@@ -13,11 +21,43 @@ public class PedidoService {
 	@Autowired
 	private PedidoRepository repo;
 	
+	@Autowired
+	private BoletoService boletoService;
+	
+	@Autowired
+	private PagamentoRepository pagamentoRepository;
+	
+	@Autowired
+	private ProdutoRepository produtoRepository;
+	
+	@Autowired
+	private ItemPedidoRepository itemPedidoRepository;
+	
 	public Pedido find(Integer id) {
 		Pedido obj = repo.findOne(id);
 		if(obj == null) {
 			throw new ObjectNotFoundException("Objeto não encontrado Id: "+ id +", Tipo: "+ Pedido.class.getName());
 		}
+		return obj;
+	}
+	
+	public Pedido insert(Pedido obj) {
+		obj.setId(null);
+		obj.setInstante(new Date());
+		obj.getPagamento().setEstado(EstadoPagamento.PENDENTE);
+		obj.getPagamento().setPedido(obj);
+		if (obj.getPagamento() instanceof PagamentoComBoleto) {
+			PagamentoComBoleto pagto = (PagamentoComBoleto) obj.getPagamento();
+			boletoService.preencherPagamentoComBoleto(pagto, obj.getInstante());
+		}
+		obj = repo.save(obj);
+		pagamentoRepository.save(obj.getPagamento());
+		for (ItemPedido ip : obj.getItens()) {
+			ip.setDesconto(0.0);
+			ip.setPreco(produtoRepository.findOne(ip.getProduto().getId()).getPreco());
+			ip.setPedido(obj);
+		}
+		itemPedidoRepository.save(obj.getItens());
 		return obj;
 	}
 }
