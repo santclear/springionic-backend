70e39bfdb1f058eb78786f3ec538a567032d2a4d
Sant'Clear <santclear@gmail.com>
2018-02-06 21:15:09 -0200

    Implementando toString de Pedido

--- src/main/java/com/santclear/services/PedidoService.java
+++ src/main/java/com/santclear/services/PedidoService.java
@@ -9,6 +9,7 @@ import com.santclear.domain.ItemPedido;
 import com.santclear.domain.PagamentoComBoleto;
 import com.santclear.domain.Pedido;
 import com.santclear.domain.enums.EstadoPagamento;
+import com.santclear.repositories.ClienteRepository;
 import com.santclear.repositories.ItemPedidoRepository;
 import com.santclear.repositories.PagamentoRepository;
 import com.santclear.repositories.PedidoRepository;
@@ -33,6 +34,9 @@ public class PedidoService {
 	@Autowired
 	private ItemPedidoRepository itemPedidoRepository;
 	
+	@Autowired
+	private ClienteRepository clienteRepository;
+	
 	public Pedido find(Integer id) {
 		Pedido obj = repo.findOne(id);
 		if(obj == null) {
@@ -44,6 +48,7 @@ public class PedidoService {
 	public Pedido insert(Pedido obj) {
 		obj.setId(null);
 		obj.setInstante(new Date());
+		obj.setCliente(clienteRepository.findOne(obj.getCliente().getId()));
 		obj.getPagamento().setEstado(EstadoPagamento.PENDENTE);
 		obj.getPagamento().setPedido(obj);
 		if (obj.getPagamento() instanceof PagamentoComBoleto) {
@@ -54,10 +59,12 @@ public class PedidoService {
 		pagamentoRepository.save(obj.getPagamento());
 		for (ItemPedido ip : obj.getItens()) {
 			ip.setDesconto(0.0);
-			ip.setPreco(produtoRepository.findOne(ip.getProduto().getId()).getPreco());
+			ip.setProduto(produtoRepository.findOne(ip.getProduto().getId()));
+			ip.setPreco(ip.getProduto().getPreco());
 			ip.setPedido(obj);
 		}
 		itemPedidoRepository.save(obj.getItens());
+		System.out.println(obj);
 		return obj;
 	}
 }
