de6456dd88f6edcc46699461911b08854bf0d6d5
Sant'Clear <santclear@gmail.com>
2018-02-13 21:04:02 -0200

    Esqueci a senha

--- src/main/java/com/santclear/resources/AuthResource.java
+++ src/main/java/com/santclear/resources/AuthResource.java
@@ -1,15 +1,19 @@
 package com.santclear.resources;
 
 import javax.servlet.http.HttpServletResponse;
+import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.santclear.dto.EmailDTO;
 import com.santclear.security.JWTUtil;
 import com.santclear.security.UserSS;
+import com.santclear.services.AuthService;
 import com.santclear.services.UserService;
 
 @RestController
@@ -19,6 +23,9 @@ public class AuthResource {
 	@Autowired
 	private JWTUtil jwtUtil;
 	
+	@Autowired
+	private AuthService service;
+	
 	// Endpoint utilizado para reaver um novo token, caso o usuário esteja usando o sistema
 	// num momento em que o token está perto de expirar
 	@RequestMapping(value = "/refresh_token", method = RequestMethod.POST)
@@ -26,6 +33,12 @@ public class AuthResource {
 		UserSS user = UserService.authenticated();
 		String token = jwtUtil.generateToken(user.getUsername());
 		response.addHeader("Authorization", "Bearer " + token);
+		return ResponseEntity.noContent().build();
+	}
+	
+	@RequestMapping(value = "/forgot", method = RequestMethod.POST)
+	public ResponseEntity<Void> forgot(@Valid @RequestBody EmailDTO objDto) {
+		service.sendNewPassword(objDto.getEmail());
 		return ResponseEntity.noContent().build();
 	}
 }
