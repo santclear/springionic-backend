8abb40c49c83884511fa4cb2194d27944dbe1940
Sant'Clear <santclear@gmail.com>
2018-02-14 20:50:44 -0200

    Salvando primeiro arquivo no S3

--- src/main/java/com/santclear/SpringIonicBackendApplication.java
+++ src/main/java/com/santclear/SpringIonicBackendApplication.java
@@ -1,16 +1,24 @@
 package com.santclear;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.santclear.services.S3Service;
+
 @SpringBootApplication
 public class SpringIonicBackendApplication implements CommandLineRunner {
+	
+	@Autowired
+	private S3Service s3Service;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(SpringIonicBackendApplication.class, args);
 	}
 
 	@Override
-	public void run(String... arg0) throws Exception {}
+	public void run(String... arg0) throws Exception {
+		s3Service.uploadFile("./temp/fotos/falcon.jpg");
+	}
 }
