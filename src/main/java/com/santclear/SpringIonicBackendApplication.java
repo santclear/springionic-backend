a8ac1dba7ecbb7782763adae15ca641830f71c1a
Sant'Clear <santclear@gmail.com>
2018-02-15 00:01:21 -0200

    Enviando imagem via endpoint

--- src/main/java/com/santclear/SpringIonicBackendApplication.java
+++ src/main/java/com/santclear/SpringIonicBackendApplication.java
@@ -1,17 +1,11 @@
 package com.santclear;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import com.santclear.services.S3Service;
-
 @SpringBootApplication
 public class SpringIonicBackendApplication implements CommandLineRunner {
-	
-	@Autowired
-	private S3Service s3Service;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(SpringIonicBackendApplication.class, args);
@@ -19,6 +13,5 @@ public class SpringIonicBackendApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... arg0) throws Exception {
-		s3Service.uploadFile("./temp/fotos/falcon.jpg");
 	}
 }
--- src/main/java/com/santclear/services/ClienteService.java
+++ src/main/java/com/santclear/services/ClienteService.java
@@ -1,5 +1,6 @@
 package com.santclear.services;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,6 +10,7 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort.Direction;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.santclear.domain.Cidade;
 import com.santclear.domain.Cliente;
@@ -36,6 +38,9 @@ public class ClienteService {
 	private CidadeRepository cidadeRepository;
 	@Autowired
 	private EnderecoRepository enderecoRepository;
+	
+	@Autowired
+	private S3Service s3Service;
 	
 	public Cliente find(Integer id) {
 		UserSS user = UserService.authenticated();
@@ -104,5 +109,9 @@ public class ClienteService {
 	private void updateData(Cliente newObj, Cliente obj) {
 		newObj.setNome(obj.getNome());
 		newObj.setEmail(obj.getEmail());
+	}
+	
+	public URI uploadProfilePicture(MultipartFile multipartFile) {
+		return s3Service.uploadFile(multipartFile);
 	}
 }
