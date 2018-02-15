52a1dd7d066355824a785cbf4450b68967ab6aa1
Sant'Clear <santclear@gmail.com>
2018-02-15 00:19:31 -0200

    Bonus: ajustando tamanho da imagem

--- src/main/java/com/santclear/services/ClienteService.java
+++ src/main/java/com/santclear/services/ClienteService.java
@@ -50,6 +50,9 @@ public class ClienteService {
 	@Value("${img.prefix.client.profile}")
 	private String prefix;
 	
+	@Value("${img.profile.size}")
+	private Integer size;
+	
 	public Cliente find(Integer id) {
 		UserSS user = UserService.authenticated();
 		// O usuário logado procurar por ele mesmo, a não ser que ele tenha perfil de ADMIN
@@ -127,6 +130,8 @@ public class ClienteService {
 		}
 		
 		BufferedImage jpgImage = imageService.getJpgImageFromFile(multipartFile);
+		jpgImage = imageService.cropSquare(jpgImage);
+		jpgImage = imageService.resize(jpgImage, size);
 		String fileName = prefix + user.getId() + ".jpg";
 		
 		return s3Service.uploadFile(imageService.getInputStream(jpgImage, "jpg"), fileName, "image");
